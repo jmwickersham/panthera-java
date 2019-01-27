3342e42e54d9a9826e481e0bbecc11ee9453a76e
Jen <jennifermwickersham@gmail.com>
2019-01-27 07:37:47 +0000

    Add PUT and DEL mappings in task controller

--- src/main/java/com/panthera/controller/TaskController.java
+++ src/main/java/com/panthera/controller/TaskController.java
@@ -1,8 +1,10 @@
 package com.panthera.controller;
 
+import com.panthera.exception.ResourceNotFoundException;
 import com.panthera.repository.TaskRepository;
 import com.panthera.model.Task;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -21,5 +23,24 @@ public class TaskController {
   @PostMapping("/tasks")
   public Task createTask(@Valid @RequestBody Task task) {
     return taskRepository.save(task);
+  }
+
+  @PutMapping("/tasks/{taskId}")
+  public Task updateTask(@PathVariable Long taskId, @Valid @RequestBody Task taskRequest) {
+    return taskRepository.findById(taskId)
+        .map(task -> {
+          task.setShort_description(taskRequest.getShort_Description());
+          task.setDescription(taskRequest.getDescription());
+          return taskRepository.save(task);
+        }).orElseThrow(() -> new ResourceNotFoundException("Task not found with id " + taskId));
+  }
+
+  @DeleteMapping("/tasks/{taskId}")
+  public ResponseEntity<?> deleteTask(@PathVariable Long taskId) {
+    return taskRepository.findById(taskId)
+        .map(task -> {
+          taskRepository.delete(task);
+          return ResponseEntity.ok().build();
+        }).orElseThrow(() -> new ResourceNotFoundException("Task not found with id " + taskId));
   }
 }
