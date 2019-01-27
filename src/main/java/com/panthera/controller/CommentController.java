c8054cabd8affb42c1835b0eb601c17f9ace6b33
Jen <jennifermwickersham@gmail.com>
2019-01-27 07:49:41 +0000

    Add PUT and DEL mappings in comment controller

--- src/main/java/com/panthera/controller/CommentController.java
+++ src/main/java/com/panthera/controller/CommentController.java
@@ -5,9 +5,12 @@ import com.panthera.repository.CommentRepository;
 import com.panthera.repository.TaskRepository;
 import com.panthera.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,5 +38,31 @@ public class CommentController {
           comment.setTask(task);
           return commentRepository.save(comment);
         }).orElseThrow(() -> new ResourceNotFoundException("Task not found with id " + taskId));
+  }
+
+  @PutMapping("/tasks/{taskId}/comments/{commentId}")
+  public Comment updateComment(@PathVariable Long taskId, @PathVariable Long commentId, @Valid @RequestBody Comment commentRequest) {
+    if (!taskRepository.existsById(taskId)) {
+      throw new ResourceNotFoundException("Task not found with id " + taskId);
+    }
+
+    return commentRepository.findById(commentId)
+        .map(comment -> {
+          commentRequest.setText(commentRequest.getText());
+          return commentRepository.save(comment);
+        }).orElseThrow(() -> new ResourceNotFoundException("Comment not found with id " + commentId));
+  }
+
+  @DeleteMapping("/tasks/{taskId}/comments/{commentid}")
+  public ResponseEntity<?> deleteComment(@PathVariable Long taskId, @PathVariable Long commentId) {
+    if (!taskRepository.existsById(taskId)) {
+      throw new ResourceNotFoundException("Task not found with id " + taskId);
+    }
+
+    return commentRepository.findById(commentId)
+        .map(comment -> {
+          commentRepository.delete(comment);
+          return ResponseEntity.ok().build();
+        }).orElseThrow(() -> new ResourceNotFoundException("Comment not found with id " + commentId));
   }
 }
